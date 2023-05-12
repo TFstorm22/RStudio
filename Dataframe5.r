2119969b6fd0c821efb84e883f97e812acf26c2c
Jose Torres Flores <115846091+TFstorm22@users.noreply.github.com>
2023-05-11 21:17:15 -0500

    Add files via upload

---
+++ Dataframe5.r
@@ -0,0 +1,9 @@
+> nombre <- c("Ana", "Juana", "Khaterine", "Hector", "Sofia")
+> sexo <- c("F", "F", "F", "M", "F")
+> edad <- c(30, 35, 28, 42, 39)
+> puesto <- c("Analista", "Gerente", "Desarrollador", "Consultor", "Administrador")
+> dni <- c("11111111", "22222222", "33333333", "44444444", "55555555")
+> 
+> empleados <- data.frame(Nombre = nombre, Sexo = sexo, Edad = edad, Puesto = puesto, Dni = dni)
+> 
+> empleados
